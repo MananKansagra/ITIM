a552d29ed95b5f22f5425cc4f56be3acab77af0b
Manan Kansagara <manandev1054@gamil.com>
2025-10-29 16:42:30 +0530

    hello.c uploaded

---
+++ hello.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * @brief The main function where program execution begins.
+ * * This program demonstrates the simplest form of C output by printing
+ * the string "Hello World" to the console.
+ *
+ * @return 0 on successful execution.
+ */
+int main(void) {
+    // The printf function is used to output formatted text to the standard output (console).
+    // The '\n' is a newline character, which moves the cursor to the next line.
+    printf("Hello World\n");
+    
+    // Return 0 indicates successful execution of the program.
+    return 0;
+}
